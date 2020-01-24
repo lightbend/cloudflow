4e41ad2dd59ff2834ebb73d09b7066bd7d21df41
Raymond Roestenburg <raymond.roestenburg@gmail.com>
2020-01-24 10:39:23 +0100

    Updated CRA example, using both Merger and Splitter in CallRecordMerge.

--- examples/call-record-aggregator/akka-cdr-ingestor/src/main/scala/carly/ingestor/CallRecordMerge.scala
+++ examples/call-record-aggregator/akka-cdr-ingestor/src/main/scala/carly/ingestor/CallRecordMerge.scala
@@ -21,6 +21,7 @@ import cloudflow.streamlets.avro._
 import cloudflow.akkastream._
 import cloudflow.akkastream.scaladsl._
 import cloudflow.akkastream.util.scaladsl.Merger
+import cloudflow.akkastream.util.scaladsl.Splitter
 
 import carly.data._
 
@@ -28,11 +29,23 @@ class CallRecordMerge extends AkkaStreamlet {
   val in0 = AvroInlet[CallRecord]("in-0")
   val in1 = AvroInlet[CallRecord]("in-1")
   val in2 = AvroInlet[CallRecord]("in-2")
-  val out = AvroOutlet[CallRecord]("out", _.user)
 
-  final override val shape = StreamletShape.withInlets(in0, in1, in2).withOutlets(out)
+  val left = AvroOutlet[InvalidRecord]("invalid", _.record)
+  val right = AvroOutlet[CallRecord]("valid", _.user)
+
+  private val oldDataWatermark = java.sql.Timestamp.valueOf("2010-01-01 00:00:00.000").getTime / 1000 //seconds
+
+  final override val shape = StreamletShape.withInlets(in0, in1, in2).withOutlets(left, right)
 
   final override def createLogic = new RunnableGraphStreamletLogic() {
-    def runnableGraph = Merger.source(in0, in1, in2).to(committableSink(out))
+    def validationFlow =
+      FlowWithCommittableContext[CallRecord]
+        .map { record ⇒
+          if (record.timestamp < oldDataWatermark) Left(InvalidRecord(record.toString, "Timestamp outside range!"))
+          else Right(record)
+        }
+
+    def runnableGraph =
+      Merger.source(in0, in1, in2).to(Splitter.sink(validationFlow, left, right))
   }
 }
