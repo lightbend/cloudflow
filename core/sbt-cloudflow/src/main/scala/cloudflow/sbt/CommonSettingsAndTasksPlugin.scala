200f299d6a4997c38b590acee401aaecc57a73f1
Chaoran Yu <yuchaoran2011@gmail.com>
2020-02-18 10:59:32 -0500

    Removed obsolete M1 string in image tags

--- core/sbt-cloudflow/src/main/scala/cloudflow/sbt/CommonSettingsAndTasksPlugin.scala
+++ core/sbt-cloudflow/src/main/scala/cloudflow/sbt/CommonSettingsAndTasksPlugin.scala
@@ -42,7 +42,7 @@ object CommonSettingsAndTasksPlugin extends AutoPlugin {
 
   // common definitions
   final val CloudflowLocalConfigFile = ".lightbend/cloudflow/pipectl.json"
-  final val CloudflowDockerBaseImage = "lightbend/cloudflow-base:1.3.0-M1-spark-2.4.4-flink-1.9.2-scala-2.12"
+  final val CloudflowDockerBaseImage = "lightbend/cloudflow-base:1.3.0-spark-2.4.4-flink-1.9.2-scala-2.12"
   // used for internal release
   final val CloudflowBintrayReleasesRepoUrl = "https://lightbend.bintray.com/cloudflow"
 
