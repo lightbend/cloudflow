599d5942720a6da35bc0eb4d661ec8946863a842
Debasish Ghosh <ghosh.debasish@gmail.com>
2020-03-06 16:18:40 +0530

    Release 1.3.1-RC4

--- installer/common/shared.sh
+++ installer/common/shared.sh
@@ -37,7 +37,7 @@ set +x
 
 # Cloudflow operator version
 export operatorImageName="lightbend/cloudflow-operator"
-export operatorImageTag="1.3.1-RC3"
+export operatorImageTag="1.3.1-RC4"
 export operatorImage="$operatorImageName:$operatorImageTag"
 
 # Kafka installation mode CloudflowManaged|ExternalStrimzi|External
