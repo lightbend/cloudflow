04f3f28d65ad91ec8eb20f09eb2f4cd2d854a5b0
Raymond Roestenburg <raymond.roestenburg@gmail.com>
2020-04-02 14:44:34 +0200

    Release 1.3.3-RC1

--- installer/common/shared.sh
+++ installer/common/shared.sh
@@ -37,7 +37,7 @@ set +x
 
 # Cloudflow operator version
 export operatorImageName="lightbend/cloudflow-operator"
-export operatorImageTag="1.3.2"
+export operatorImageTag="1.3.3-RC1"
 export operatorImage="$operatorImageName:$operatorImageTag"
 
 # Kafka installation mode CloudflowManaged|ExternalStrimzi|External
