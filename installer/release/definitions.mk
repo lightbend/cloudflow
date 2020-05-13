7279b5e43130c051631262039fa0f86073cebc38
Chaoran Yu <yuchaoran2011@gmail.com>
2020-05-12 21:56:46 -0400

    Updated spark operator image tag

--- installer/release/definitions.mk
+++ installer/release/definitions.mk
@@ -2,4 +2,4 @@ version := 1.3.4-SNAPSHOT
 cloudflow_installer_image_tag := 1.3.4-SNAPSHOT
 cloudflow_operator_image_tag := 1.3.4-SNAPSHOT
 
-cloudflow_spark_operator_image := 1.3.1-OpenJDK-2.4.5-1.1.0-cloudflow-2.12
+cloudflow_spark_operator_image := 1.3.3-OpenJDK-2.4.5-1.1.0-cloudflow-2.12
