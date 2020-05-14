f89780fb75f11d5c9d0d7b50c8a6c1dbfb56d06d
Raymond Roestenburg <raymond.roestenburg@gmail.com>
2020-05-14 15:45:35 +0200

    Updated to alpakka kafka 2.0.3. (#413)
    * Updated to alpakka kafka 2.0.3.
    
    * newline.

--- core/project/Dependencies.scala
+++ core/project/Dependencies.scala
@@ -5,7 +5,7 @@ object Version {
   val Akka         = "2.6.5"
   val AkkaHttp     = "10.1.11"
   val AkkaMgmt     = "1.0.6"
-  val AlpakkaKafka = "2.0.2"
+  val AlpakkaKafka = "2.0.3"
   val Scala        = "2.12.9"
   val Spark        = "2.4.5"
   val Flink        = "1.10.0"
