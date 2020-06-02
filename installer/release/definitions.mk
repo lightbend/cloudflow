f06862d6e793073927eeb217437b13163effd4d3
Chaoran Yu <yuchaoran2011@gmail.com>
2020-06-02 09:13:47 -0400

    Changed version to 2.0.0-SNAPSHOT. Reverted change in swiss-knife json

--- installer/release/definitions.mk
+++ installer/release/definitions.mk
@@ -1,5 +1,5 @@
-version := 2.0.0-rc1
-cloudflow_installer_image_tag := 2.0.0-rc1
-cloudflow_operator_image_tag := 2.0.0-rc1
+version := 2.0.0-SNAPSHOT
+cloudflow_installer_image_tag := 2.0.0-SNAPSHOT
+cloudflow_operator_image_tag := 2.0.0-SNAPSHOT
 
 cloudflow_spark_operator_image := 1.3.3-OpenJDK-2.4.5-1.1.0-cloudflow-2.12
