0011198f9fd4cbc9ff9e68cf40711e88fea415d3
Raymond Roestenburg <raymond.roestenburg@gmail.com>
2020-07-02 17:01:23 +0200

    Updated 2.0.0 to 2.0.4 in installer.

--- installer/release/definitions.mk
+++ installer/release/definitions.mk
@@ -1,9 +1,9 @@
-version := 2.0.0
+version := 2.0.4
 
 cloudflow_installer_image_tag := 491-f242fe8-dirty
 
 cloudflow_operator_image_name := lightbend/cloudflow-operator
-cloudflow_operator_image_tag := 2.0.0
+cloudflow_operator_image_tag := 2.0.4
 cloudflow_spark_operator_image_name := lightbend/sparkoperator
 cloudflow_spark_operator_image_tag := 1.3.3-OpenJDK-2.4.5-1.1.0-cloudflow-2.12
 cloudflow_flink_operator_image_name := lyft/flinkk8soperator
