95dd77ad1d68d0e95bcb6ff4a05113738de9f128
Nolan Grace <nolan.grace@lightbend.com>
2020-07-02 19:32:27 -0500

    clean up akka actor

--- examples/connected-car-cluster-sharding/akka-connected-car-streamlet/src/main/scala/connectedcar/actors/ConnectedCarActor.scala
+++ examples/connected-car-cluster-sharding/akka-connected-car-streamlet/src/main/scala/connectedcar/actors/ConnectedCarActor.scala
@@ -11,17 +11,16 @@ object ConnectedCarActor {
   def apply(carId:String): Behavior[ConnectedCarERecordWrapper] = {
     def updated(numberOfRecords: Int, driverName: String, carId:String, averageSpeed: Double, currentSpeed: Double): Behavior[ConnectedCarERecordWrapper] = {
       Behaviors.receive { (context, message) => {
-        context.log.info("Updated CarId: " + carId +
-          " Driver Name: " + driverName + " CarSpeed: " + currentSpeed + " From Actor:" + message.sender.path)
+          context.log.info("Update Received- CarId: "+carId+" MessageCarId: "+message.record.carId +
+            " From Actor:" + message.sender.path)
 
+          val newAverage = ((averageSpeed * numberOfRecords) + message.record.speed) / (numberOfRecords + 1)
+          val newNumberOfRecords = numberOfRecords+1
 
-        val newAverage = ((averageSpeed * numberOfRecords) + message.record.speed) / (numberOfRecords + 1)
-        val newNumberOfRecords = numberOfRecords+1
+          message.sender ! ConnectedCarAgg(message.record.carId, message.record.driver, averageSpeed, newNumberOfRecords)
 
-        message.sender ! ConnectedCarAgg(message.record.carId, message.record.driver, averageSpeed, newNumberOfRecords)
-
-        updated(newNumberOfRecords, message.record.driver, message.record.carId+"", newAverage, message.record.speed)
-      }
+          updated(newNumberOfRecords, message.record.driver, carId, newAverage, message.record.speed)
+        }
       }
     }
 
