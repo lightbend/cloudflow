0a61bf66ebce971f51a723f4803663643d9b8062
Raymond Roestenburg <raymond.roestenburg@gmail.com>
2020-07-13 21:03:26 +0200

    Release 2.0.6-RC6

--- installer/deprecated-installer/common/shared.sh
+++ installer/deprecated-installer/common/shared.sh
@@ -37,7 +37,7 @@ set +x
 
 # Cloudflow operator version
 export operatorImageName="lightbend/cloudflow-operator"
-export operatorImageTag="2.0.6-RC4"
+export operatorImageTag="2.0.6-RC6"
 export operatorImage="$operatorImageName:$operatorImageTag"
 
 # Kafka installation mode CloudflowManaged|ExternalStrimzi|External
