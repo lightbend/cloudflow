84d39dae097074038e959bc880b0aaa25544ea7a
Chaoran Yu <yuchaoran2011@gmail.com>
2020-07-17 05:43:23 -0400

    Fixed a permission issue in Flink entrypoint; Upgraded Flink operator to 0.5.0 with Go 1.14 (#587)

--- external/README.md
+++ external/README.md
@@ -3,9 +3,10 @@
 The current published artifacts are:
 
 * [Spark distribution file](https://github.com/lightbend/spark/releases/download/2.4.5-lightbend/spark-2.4.5-bin-cloudflow-2.12.tgz).
-* `lightbend/sparkoperator:2.0.6-OpenJDK-2.4.5-1.1.0-cloudflow-2.12`
+* `lightbend/sparkoperator:2.0.6-cloudflow-spark-2.4.5-1.1.2-scala-2.12`
 * `lightbend/spark:2.0.6-OpenJDK-2.4.5-cloudflow-2.12`
-* `lightbend/flink:2.0.6-cloudflow-flink-1.10.0-scala-2.12 `
+* `lightbend/flink:2.0.6-cloudflow-flink-1.10.0-scala-2.12`
+* `lightbend/akka-base:2.0.6-cloudflow-akka-2.6.6-scala-2.12`
 * Cloudflow jars: https://lightbend.bintray.com/cloudflow.
 * Cloudflow `kubectl` plugin: https://bintray.com/lightbend/cloudflow-cli.
 
--- installer/src/test/scala/com/lightbend/cloudflow/installer/TestInstance.scala
+++ installer/src/test/scala/com/lightbend/cloudflow/installer/TestInstance.scala
@@ -7,7 +7,7 @@ object TestInstance {
 
   def get(): CustomResource[Spec, Status] = {
     val kafkaClusterCr    = KafkaClusterCR("strimzi", "0.16.2", "gp2", "gp2", "strimzi/kafka", "0.16.2-kafka-2.4.0", "strimzi/operator", "0.16.2")
-    val flinkOperator     = FlinkOperator("0.8.2", "flink-service-account", "lyft/flinkk8soperator", "v0.4.0")
+    val flinkOperator     = FlinkOperator("0.8.2", "flink-service-account", "lightbend/flinkk8soperator", "v0.5.0")
     val sparkOperator     = SparkOperator("0.6.7", "lightbend/sparkoperator", "2.0.6-cloudflow-spark-2.4.5-1.1.2-scala-2.12", "alpine:3.9")
     val cloudflowOperator = CloudflowOperator("lightbend/cloudflow-operator", "2.0.0", "glusterfs-storage")
     val spec = Spec(
