cc02d17d3518ae2184724cd17b34b1845443fe6e
Nolan Grace <nolan.grace@lightbend.com>
2020-08-12 14:08:39 -0500

    Update core/cloudflow-akka/src/main/scala/cloudflow/akkastream/AkkaStreamletLogic.scala
    Co-authored-by: Raymond Roestenburg <raymond.roestenburg@gmail.com>

--- core/cloudflow-akka/src/main/scala/cloudflow/akkastream/AkkaStreamletLogic.scala
+++ core/cloudflow-akka/src/main/scala/cloudflow/akkastream/AkkaStreamletLogic.scala
@@ -149,7 +149,7 @@ abstract class AkkaStreamletLogic(implicit val context: AkkaStreamletContext) ex
    * Akka Cluster Sharding using the supplied `shardEntity` and configure the kafka external
    * shard strategy to co-locate Kafka partition consumption with Akka Cluster shards.
    *
-   * @param inlet specifies a [[cloudflow.streamlets.Codec]] that is used to deserialize the records read from the underlying transport.
+   * @param inlet the inlet to consume messages from. The inlet specifies a [[cloudflow.streamlets.Codec]] that is used to deserialize the records read from the underlying transport.
    * @param shardEntity is used to specific the settings for the started shard region
    **/
   @ApiMayChange
