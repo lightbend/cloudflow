be1416edffbff6c19e70d7d5f9b46635e21bd7bc
Andrea Peruffo <andrea.peruffo1982@gmail.com>
2020-11-06 10:49:41 +0000

    test for the CI

--- docs/build-javascaladocs.sh
+++ docs/build-javascaladocs.sh
@@ -2,16 +2,18 @@
 
 VERSION=$1
 
-(cd target && \
-	rm -rf cloudflow && \
-	git clone --depth=100 --branch "$VERSION" https://github.com/lightbend/cloudflow.git && \
-  cd cloudflow && \
-  export DIR=$(yq r docs/docs-source/docs/antora.yml 'version') && \
-  echo $DIR && \
-	cd core && \
-	(sbt -mem 2048 clean unidoc | true) && \
-  cd ../../../ && \
-	mkdir -p "./target/staging/docs/$DIR/api/scaladoc" && \
-	mkdir -p "./target/staging/docs/$DIR/api/javadoc" && \
-	cp -r "./target/cloudflow/core/target/scala-2.12/unidoc/" "./target/staging/docs/$DIR/api/scaladoc" && \
-	cp -r "./target/cloudflow/core/target/javaunidoc/" "./target/staging/docs/$DIR/api/javadoc")
+echo $VERSION
+
+# (cd target && \
+# 	rm -rf cloudflow && \
+# 	git clone --depth=100 --branch "$VERSION" https://github.com/lightbend/cloudflow.git && \
+#   cd cloudflow && \
+#   export DIR=$(yq r docs/docs-source/docs/antora.yml 'version') && \
+#   echo $DIR && \
+# 	cd core && \
+# 	(sbt -mem 2048 clean unidoc | true) && \
+#   cd ../../../ && \
+# 	mkdir -p "./target/staging/docs/$DIR/api/scaladoc" && \
+# 	mkdir -p "./target/staging/docs/$DIR/api/javadoc" && \
+# 	cp -r "./target/cloudflow/core/target/scala-2.12/unidoc/" "./target/staging/docs/$DIR/api/scaladoc" && \
+# 	cp -r "./target/cloudflow/core/target/javaunidoc/" "./target/staging/docs/$DIR/api/javadoc")
