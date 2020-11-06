a78607c9cc746fee6bfc16bb9c5313fcab7c34e5
Andrea Peruffo <andrea.peruffo1982@gmail.com>
2020-11-06 12:27:15 +0000

    test

--- docs/build-javascaladocs.sh
+++ docs/build-javascaladocs.sh
@@ -4,16 +4,20 @@ VERSION=$1
 
 echo $VERSION
 
-(cd target && \
-	rm -rf cloudflow && \
-	git clone --depth=100 --branch "$VERSION" https://github.com/lightbend/cloudflow.git && \
-  cd cloudflow && \
-  export DIR=$(yq r docs/docs-source/docs/antora.yml 'version') && \
-  echo $DIR && \
-	cd core && \
-	(sbt -mem 2048 clean unidoc | true) && \
-  cd ../../../ && \
-	mkdir -p "./target/staging/docs/$DIR/api/scaladoc" && \
-	mkdir -p "./target/staging/docs/$DIR/api/javadoc" && \
-	cp -r "./target/cloudflow/core/target/scala-2.12/unidoc/" "./target/staging/docs/$DIR/api/scaladoc" && \
-	cp -r "./target/cloudflow/core/target/javaunidoc/" "./target/staging/docs/$DIR/api/javadoc")
+if [[ -z "${VERSION}" ]]; then
+  echo "Version not defined"
+else
+  (cd target && \
+    rm -rf cloudflow && \
+    git clone --depth=100 --branch "$VERSION" https://github.com/lightbend/cloudflow.git && \
+    cd cloudflow && \
+    export DIR=$(yq r docs/docs-source/docs/antora.yml 'version') && \
+    echo $DIR && \
+    cd core && \
+    (sbt -mem 2048 clean unidoc | true) && \
+    cd ../../../ && \
+    mkdir -p "./target/staging/docs/$DIR/api/scaladoc" && \
+    mkdir -p "./target/staging/docs/$DIR/api/javadoc" && \
+    cp -r "./target/cloudflow/core/target/scala-2.12/unidoc/" "./target/staging/docs/$DIR/api/scaladoc" && \
+    cp -r "./target/cloudflow/core/target/javaunidoc/" "./target/staging/docs/$DIR/api/javadoc")
+fi
