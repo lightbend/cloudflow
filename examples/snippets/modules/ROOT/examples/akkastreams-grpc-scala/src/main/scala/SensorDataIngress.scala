5b03af81463da620a68984213183be921983f33d
Arnout Engelen <arnout@bzzt.net>
2020-11-24 11:38:29 +0100

    Add more context to gRPC example (#892)

--- examples/snippets/modules/ROOT/examples/akkastreams-grpc-scala/src/main/scala/SensorDataIngress.scala
+++ examples/snippets/modules/ROOT/examples/akkastreams-grpc-scala/src/main/scala/SensorDataIngress.scala
@@ -1,18 +1,30 @@
 package sensordata
 
-import akka.grpc.scaladsl.{ ServerReflection, ServiceHandler }
+//tag::logic[]
+import akka.grpc.scaladsl.ServerReflection
+//end::logic[]
 import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.server.Route
 import akka.http.scaladsl.server.directives.RouteDirectives
 
 import cloudflow.akkastream._
+//tag::logic[]
 import cloudflow.akkastream.util.scaladsl.GrpcServerLogic
+//end::logic[]
 import cloudflow.streamlets._
 import cloudflow.streamlets.proto.ProtoOutlet
 
+//tag::logic[]
 import sensordata.grpc.{ SensorData, SensorDataService, SensorDataServiceHandler }
 
+//end::logic[]
+
+//tag::logic[]
 class SensorDataIngress extends AkkaServerStreamlet {
+  // ...
+
+  //end::logic[]
+
   val out   = ProtoOutlet[SensorData]("out", RoundRobinPartitioner)
   def shape = StreamletShape.withOutlets(out)
 
