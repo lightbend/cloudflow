afbcc10c63d4e7dbbdf8c2685343bbfc0a87f9ad
Andrea Peruffo <andrea.peruffo1982@gmail.com>
2020-12-16 17:31:50 +0000

    wip whitesource integration

--- core/project/WhitesourceLicensePlugin.scala
+++ core/project/WhitesourceLicensePlugin.scala
@@ -1,16 +1,50 @@
+// import sbt._
+
+// import sbtwhitesource.WhiteSourcePlugin.autoImport._
+
+// object LicensePlugin extends AutoPlugin {
+//   override def requires = plugins.JvmPlugin
+//   override def trigger  = allRequirements
+
+//   override def buildSettings: Seq[Setting[_]] =
+//     List(
+//       whitesourceProduct := "cloudflow",
+//       whitesourceAggregateProjectName := "cloudflow-streamlets-1.0-stable",
+//       // The project token is just a project UID, not a password.
+//       whitesourceAggregateProjectToken := "7a76fc644e2d4ffc8a1b8ca9a9406e78624994604c6f43fc8bdbbda4a188d62a"
+//     )
+// }
+
 import sbt._
-
+import sbt.Keys._
 import sbtwhitesource.WhiteSourcePlugin.autoImport._
+import sbtwhitesource._
+import scala.sys.process.Process
+import scala.util.Try
 
-object LicensePlugin extends AutoPlugin {
-  override def requires = plugins.JvmPlugin
-  override def trigger  = allRequirements
+object Whitesource extends AutoPlugin {
+  private lazy val gitCurrentBranch =
+    Try(Process("git rev-parse --abbrev-ref HEAD").!!.replaceAll("\\s", "")).recover {
+      case e => sys.error(s"Couldn't determine git branch for Whitesource: $e")
+    }.toOption
 
-  override def buildSettings: Seq[Setting[_]] =
-    List(
-      whitesourceProduct := "cloudflow",
-      whitesourceAggregateProjectName := "cloudflow-streamlets-1.0-stable",
-      // The project token is just a project UID, not a password.
-      whitesourceAggregateProjectToken := "7a76fc644e2d4ffc8a1b8ca9a9406e78624994604c6f43fc8bdbbda4a188d62a"
-    )
+  override def requires = WhiteSourcePlugin
+
+  override def trigger = allRequirements
+
+  override lazy val projectSettings = Seq(
+    // do not change the value of whitesourceProduct
+    whitesourceProduct := "cloudflow",
+    whitesourceAggregateProjectName := {
+      "cloudflow-2.0-" + (
+        if (isSnapshot.value)
+          if (gitCurrentBranch.contains("master")) "master"
+          else "adhoc"
+        else majorMinor((LocalRootProject / version).value).map(_ + "-stable").getOrElse("adhoc")
+      )
+    },
+    whitesourceForceCheckAllDependencies := true,
+    whitesourceFailOnError := true)
+
+  private def majorMinor(version: String): Option[String] = """\d+\.\d+""".r.findFirstIn(version)
 }
--- core/project/plugins.sbt
+++ core/project/plugins.sbt
@@ -6,7 +6,7 @@ addSbtPlugin("com.eed3si9n"      % "sbt-buildinfo"        % "0.9.0")
 addSbtPlugin("net.virtual-void"  % "sbt-dependency-graph" % "0.9.2")
 addSbtPlugin("com.julianpeeters" % "sbt-avrohugger"       % "2.0.0-RC18")
 addSbtPlugin("com.lightbend.sbt" % "sbt-javaagent"        % "0.1.5")
-addSbtPlugin("com.lightbend"     % "sbt-whitesource"      % "0.1.16")
+addSbtPlugin("com.lightbend"     % "sbt-whitesource"      % "0.1.18")
 
 addSbtPlugin("se.marcuslonnberg" % "sbt-docker"          % "1.8.0")
 addSbtPlugin("com.typesafe.sbt"  % "sbt-native-packager" % "1.3.24")
