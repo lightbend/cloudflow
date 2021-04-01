a7ccea9853fbeed91f625f18f8eb88675a819fc1
Andrea Peruffo <andrea.peruffo1982@gmail.com>
2021-04-01 10:59:30 +0100

    minor

--- tools/cloudflow-descriptor-generator/src/test/scala/akka/cloudflow/TestStreamlets.scala
+++ tools/cloudflow-descriptor-generator/src/test/scala/akka/cloudflow/TestStreamlets.scala
@@ -16,77 +16,13 @@
 
 package akka.cloudflow
 
-//import org.apache.avro.{ Schema, SchemaBuilder }
-
 import com.typesafe.config.Config
 import cloudflow.streamlets._
-//import cloudflow.streamlets.avro.AvroUtil
 
 case class Coffee(espressos: Int)
 case class Dollars(amount: Double)
 case class Gold(kg: Double)
 case class Bar(karat: Int)
-
-object Ports {
-//  def outletFor(schema: Schema) = new Outlet() {
-//    def name             = "out"
-//    def schemaDefinition = AvroUtil.createSchemaDefinition(schema)
-//  }
-//
-//  def inletFor(schema: Schema) = new Inlet() {
-//    def name             = "in"
-//    def schemaDefinition = AvroUtil.createSchemaDefinition(schema)
-//  }
-}
-
-//object Schemas {
-//  val coffeeSchema = SchemaBuilder
-//    .record("Coffee")
-//    .namespace("cloudflow.sbt")
-//    .fields()
-//    .name("expressos")
-//    .`type`()
-//    .nullable()
-//    .intType()
-//    .noDefault()
-//    .endRecord()
-//  val dollarSchema = SchemaBuilder
-//    .record("Dollars")
-//    .namespace("cloudflow.sbt")
-//    .fields()
-//    .name("amount")
-//    .`type`()
-//    .nullable()
-//    .doubleType()
-//    .noDefault()
-//    .endRecord()
-//  val barSchema = SchemaBuilder
-//    .record("Bar")
-//    .namespace("cloudflow.sbt")
-//    .fields()
-//    .name("karat")
-//    .`type`()
-//    .nullable()
-//    .intType()
-//    .noDefault()
-//    .endRecord()
-//
-//}
-
-//case object TestRuntime extends StreamletRuntime {
-//  override val name = "test-runtime"
-//}
-
-//trait TestStreamlet extends Streamlet[StreamletContext] {
-//  override def createContext(config: Config): StreamletContext    = ???
-//  override def run(context: StreamletContext): StreamletExecution = ???
-//  override def runtime: StreamletRuntime                          = TestRuntime
-//  def logStartRunnerMessage(buildInfo: String): Unit              = ???
-//}
-//
-//abstract class AbstractStreamlet() extends TestStreamlet {
-//  override def shape() = StreamletShape(Ports.inletFor(Schemas.dollarSchema), Ports.outletFor(Schemas.barSchema))
-//}
 
 trait TestStreamlet extends Streamlet {}
 
@@ -96,43 +32,19 @@ abstract class AbstractStreamlet() extends TestStreamlet {}
 // Valid Streamlets
 // ===============================
 
-//class CoffeeIngress extends TestStreamlet {
-//  override def shape()     = StreamletShape(Ports.outletFor(Schemas.coffeeSchema))
-//  override val labels      = Vector("test", "coffee")
-//  override val description = "Coffee Ingress Test"
-//}
-//
-//class CodeFlow extends TestStreamlet {
-//  override def shape() = StreamletShape(Ports.inletFor(Schemas.coffeeSchema), Ports.outletFor(Schemas.dollarSchema))
-//}
-//
-//object BarFlow extends TestStreamlet {
-//  override def shape() = StreamletShape(Ports.inletFor(Schemas.dollarSchema), Ports.outletFor(Schemas.barSchema))
-//}
+class CoffeeIngress extends TestStreamlet {}
 
-class CoffeeIngress extends TestStreamlet {
-//  override def shape()     = StreamletShape(Ports.outletFor(Schemas.coffeeSchema))
-//  override val labels      = Vector("test", "coffee")
-//  override val description = "Coffee Ingress Test"
-}
+class CodeFlow extends TestStreamlet {}
 
-class CodeFlow extends TestStreamlet {
-//  override def shape() = StreamletShape(Ports.inletFor(Schemas.coffeeSchema), Ports.outletFor(Schemas.dollarSchema))
-}
-
-object BarFlow extends TestStreamlet {
-//  override def shape() = StreamletShape(Ports.inletFor(Schemas.dollarSchema), Ports.outletFor(Schemas.barSchema))
-}
+object BarFlow extends TestStreamlet {}
 
 class ClassWithCompanionObject {
   def foo = "bar"
 }
-object ClassWithCompanionObject extends TestStreamlet {
-//  override def shape() = StreamletShape(Ports.inletFor(Schemas.dollarSchema), Ports.outletFor(Schemas.barSchema))
-}
+
+object ClassWithCompanionObject extends TestStreamlet {}
 
 class BarFlowWithConfig extends TestStreamlet {
-//  override def shape()          = StreamletShape(Ports.inletFor(Schemas.dollarSchema), Ports.outletFor(Schemas.barSchema))
   val GoldPrice = ConfigParameter("gold-price", "the dollar price of gold (1gram)")
   override def configParameters = Vector(GoldPrice)
 }
@@ -143,11 +55,8 @@ class BarFlowWithConfig extends TestStreamlet {
 
 class NoDefaultConstructorStreamlet(noDefArgs: String) extends TestStreamlet {
   def useArgs = noDefArgs + noDefArgs
-
-//  override def shape() = StreamletShape(Ports.inletFor(Schemas.dollarSchema), Ports.outletFor(Schemas.barSchema))
 }
 
 class StreamletThatThrowsAnExceptionInItsConstructor extends TestStreamlet {
   throw new RuntimeException("Boom! I will NEVER be instantiated!!")
-//  override def shape() = StreamletShape(Ports.inletFor(Schemas.coffeeSchema), Ports.outletFor(Schemas.dollarSchema))
 }
