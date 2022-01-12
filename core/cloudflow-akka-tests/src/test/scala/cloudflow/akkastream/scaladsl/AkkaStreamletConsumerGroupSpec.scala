dba9dcf4e5c2e96e7e13154e9423609df763369d
Raymond Roestenburg <raymond.roestenburg@gmail.com>
2022-01-12 13:53:43 +0100

    'Fix' for flaky test.

--- core/cloudflow-akka-tests/src/test/scala/cloudflow/akkastream/scaladsl/AkkaStreamletConsumerGroupSpec.scala
+++ core/cloudflow-akka-tests/src/test/scala/cloudflow/akkastream/scaladsl/AkkaStreamletConsumerGroupSpec.scala
@@ -52,7 +52,7 @@ class AkkaStreamletConsumerGroupSpec extends TestcontainersKafkaSpec(ActorSystem
   "Akka streamlet instances" should {
     "consume from an outlet as a group per streamlet reference" in {
       // Generate some test data
-      val dataSize = 5000
+      val dataSize = 10000
       val data = List.range(0, dataSize).map(i => Data(i, s"data"))
       val outlet = mkUniqueGenOutlet()
       val genExecution = Generator.run(data, outlet)
@@ -72,7 +72,7 @@ class AkkaStreamletConsumerGroupSpec extends TestcontainersKafkaSpec(ActorSystem
       }
 
       // verify the data that the test receiver instances have processed.
-      val receivedData = probe.receiveN(dataSize, 15.seconds)
+      val receivedData = probe.receiveN(dataSize, 30.seconds)
       probe.expectNoMessage()
 
       // verify that all receiver instances together have exactly received all the test data.
