a7a333de29e753597a5fde4d4bee4dd20f5f4941
Raymond Roestenburg <raymond.roestenburg@gmail.com>
2022-01-17 21:01:18 +0100

    Added comment on failing docs build for cli.

--- core/project/Dependencies.scala
+++ core/project/Dependencies.scala
@@ -25,7 +25,7 @@ object Dependencies {
 
     val typesafeConfig = "com.typesafe" % "config" % "1.4.1"
     val pureConfig = "com.github.pureconfig" %% "pureconfig" % "0.14.0"
-    val scopt = "com.github.scopt" %% "scopt" % "4.0.1"
+    val scopt = "com.github.scopt" %% "scopt" % "4.0.1" // FIXME generating docs for CLI fails with concurrent modification with 2.13 and this version.
     val airframeLog = "org.wvlet.airframe" %% "airframe-log" % "20.10.0"
     val asciiTable = "de.vandermeer" % "asciitable" % "0.3.2"
 
