f0d99494ff94c6727ac3c34bd3d01cd5cfc35609
Raymond Roestenburg <raymond.roestenburg@gmail.com>
2022-01-19 16:20:03 +0100

    Removed spark example leftover.

--- examples/snippets/modules/ROOT/examples/spark-scala/src/main/scala/sensors/Streamlets.scala
+++
@@ -1,5 +0,0 @@
-package sensors
-
-class SparkRandomGenDataIngress extends cloudflow.sparkdoc.SparkRandomGenDataIngress
-class MovingAverageSparklet     extends cloudflow.sparkdoc.MovingAverageSparklet
-class SparkConsoleEgress        extends cloudflow.sparkdoc.SparkConsoleEgress
