77026327aac7fe9808dcc3f4b584d54521adf325
Scala Steward <me@scala-steward.org>
2022-04-04 12:47:02 +0200

    Update scalatest, scalatest-mustmatchers to 3.2.11

--- core/project/Dependencies.scala
+++ core/project/Dependencies.scala
@@ -17,7 +17,7 @@ object Dependencies {
     val jackson = "2.13.2"
     val jacksonDatabind = "2.13.2.2"
     val slf4j = "1.7.30"
-    val scalaTest = "3.2.3"
+    val scalaTest = "3.2.11"
     val maven = "3.8.1"
   }
 
