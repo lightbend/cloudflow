73f15d61a3a535e8d7a28ff0d7b40e1a24b6573f
Scala Steward <me@scala-steward.org>
2022-04-29 09:39:48 +0200

    Revert commit(s) e398b6bf

--- core/project/Dependencies.scala
+++ core/project/Dependencies.scala
@@ -9,7 +9,7 @@ object Dependencies {
   object Versions {
     val akka = "2.6.18"
     val akkaHttp = "10.2.7"
-    val akkaGrpc = "2.1.4"
+    val akkaGrpc = "2.1.3"
     val alpakkaKafka = "2.1.1"
     val akkaMgmt = "1.0.8"
     val spark = "2.4.5"
