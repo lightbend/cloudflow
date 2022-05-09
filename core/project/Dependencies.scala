3733e5bdc3b3a73324a6467de028e45dc944dbd8
Scala Steward <me@scala-steward.org>
2022-05-10 00:31:01 +0200

    Update kafka-clients to 3.2.0

--- core/project/Dependencies.scala
+++ core/project/Dependencies.scala
@@ -91,7 +91,7 @@ object Dependencies {
     val ficus = "com.iheart" %% "ficus" % "1.5.2"
 
     val kubeActions = "com.lightbend.akka" %% "kube-actions" % "0.1.1"
-    val kafkaClient = "org.apache.kafka" % "kafka-clients" % "3.1.0"
+    val kafkaClient = "org.apache.kafka" % "kafka-clients" % "3.2.0"
 
     val classgraph = "io.github.classgraph" % "classgraph" % "4.8.146"
 
