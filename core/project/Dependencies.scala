65796a335609beb31b8a766c37450ca97779bc87
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2022-08-08 10:54:26 +0200

    Update kafka to 1.17.3 (#1223)

--- core/project/Dependencies.scala
+++ core/project/Dependencies.scala
@@ -97,7 +97,7 @@ object Dependencies {
     val classgraph = "io.github.classgraph" % "classgraph" % "4.8.149"
 
     val scalaPbCompilerPlugin = "com.thesamet.scalapb" %% "compilerplugin" % scalapb.compiler.Version.scalapbVersion
-    val testcontainersKafka = "org.testcontainers" % "kafka" % "1.17.2"
+    val testcontainersKafka = "org.testcontainers" % "kafka" % "1.17.3"
     val asciigraphs = "com.github.mutcianm" %% "ascii-graphs" % "0.0.6"
 
     val mavenPluginApi = "org.apache.maven" % "maven-plugin-api" % Versions.maven
