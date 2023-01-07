8c74f7f559f600d5063d7f9457ffb65dc1bc0495
Scala Steward <scala_steward@virtuslab.com>
2023-01-07 20:15:39 +0000

    Update scalatest, scalatest-mustmatchers to 3.2.15

--- core/project/Dependencies.scala
+++ core/project/Dependencies.scala
@@ -18,7 +18,7 @@ object Dependencies {
     //TODO remove jacksonDatabind when jackson 2.13.3 plus excludes in avro and jacksonScala
     val jacksonDatabind = "2.14.1"
     val slf4j = "1.7.30"
-    val scalaTest = "3.2.14"
+    val scalaTest = "3.2.15"
     val maven = "3.8.5"
   }
 
